ea9edd812fbc869bc88d3f3f24577214b5214b28
Christian Lee <christian.r.lee@gmail.com>
2019-11-04 14:24:48 -0500

    Create group_pupil_data.m

---
+++ data-analysis/group_pupil_data.m
@@ -0,0 +1,12 @@
+folder = uigetdir;
+cd(folder);
+filePattern = fullfile(folder, '*.mat');
+matfiles = dir(filePattern);
+count = length(matfiles);
+keepercol = 1;
+for f = 1:count;
+    B = matfiles(f, 1).name;
+    currkeeper = load(B);
+    name = char(fieldnames(currkeeper));
+    holdercells(1, f) = {currkeeper.(name)};
+end
