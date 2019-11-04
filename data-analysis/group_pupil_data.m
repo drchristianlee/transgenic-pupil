d3b942e365d0333af0c04e9b6c6cb53fd446248d
Christian Lee <christian.r.lee@gmail.com>
2019-11-04 14:43:01 -0500

    Update group_pupil_data.m

--- data-analysis/group_pupil_data.m
+++ data-analysis/group_pupil_data.m
@@ -1,3 +1,11 @@
+% Use this script to analyze group data. Create a folder, in this case
+% named group data. Within that folder create two other folders, in this
+% case named intensity_mW_1 for 1 mW intensity and intensity_mW_1_8 for 1.8
+% mW. Within those folders create two more folders named transgenic_flox
+% and transgenic_null. Copy and paste diameter data for the corresponding
+% mice into each folder. After pasting rename files so they are not
+% overwritten using a naming scheme. 
+
 folder = uigetdir;
 cd(folder);
 filePattern = fullfile(folder, '*.mat');
