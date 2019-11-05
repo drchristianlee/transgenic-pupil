c45357dea30a5f83f5e69b09b6a562a9e31b6840
Christian Lee <christian.r.lee@gmail.com>
2019-11-05 09:53:26 -0500

    Update group_pupil_data.m

--- data-analysis/group_pupil_data.m
+++ data-analysis/group_pupil_data.m
@@ -2,7 +2,7 @@
 % named group data. Within that folder create two other folders, in this
 % case named intensity_mW_1 for 1 mW intensity and intensity_mW_1_8 for 1.8
 % mW. Within those folders create two more folders named transgenic_flox
-% and transgenic_null. Copy and paste diameter data for the corresponding
+% and transgenic_null. Copy and paste avgsem.mat data for the corresponding
 % mice into each folder. After pasting rename files so they are not
 % overwritten using a naming scheme. 
 
